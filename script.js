b96d202d10f40a7a3d9bf65062a1eac5483bd106
s-j-n78 <120406961+s-j-n78@users.noreply.github.com>
2023-03-10 17:55:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const firebaseConfig = {
+    
+	 apiKey: "AIzaSyCxvXV-ETjFpuQBWnXT19oz1B0AdaCt-Vk",
+    authDomain: "expanded-future-380213.firebaseapp.com",
+    projectId: "expanded-future-380213",
+    storageBucket: "expanded-future-380213.appspot.com",
+    messagingSenderId: "20696568619",
+    appId: "1:20696568619:web:8b11f38307669f8a1beb6e"
+	
+  };
+  
+	firebase.initializeApp(firebaseConfig);
+
+
+let queryURL = "https://www.googleapis.com/fitness/v1/users/me/dataSources"
+
+fetch(queryURL)
+.then((response) => {
+	return response.json()
+})
+.then((data) => {
+	console.log(data)
+	
+})
+.catch((err) => {
+	console.log(err)
+	
+})
+
+
+
+
+
 let y = 3;
 let x = (100 + 50) * y;
 
